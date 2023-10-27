869b5a653c867a098d807f8a5cdba0e93f8cf4b1
Abraar <reinhardt.codes@gmail.com>
2023-10-27 21:08:34 +0600

    update abassa

--- Python Stuff/For Abassa/list_to_string.py
+++ Python Stuff/For Abassa/list_to_string.py
@@ -9,6 +9,23 @@ def list_to_string(items: List[Union[str, int]]) -> str:
     return ', '.join(str(item) for item in items[:-1]) + ', and ' + str(items[-1])
 
 
+def list_to_string1(items):
+
+    if not items:
+        return ""
+
+    if len(items) == 1:
+        return str(items[0])
+
+    # Convert all items in the list to strings, except for the last one
+    items_str = ', '.join(str(item) for item in items[:-1])
+
+    # Add the word 'and' before the last item
+    items_str += ', and ' + str(items[-1])
+
+    return items_str
+
+
 if __name__ == '__main__':
 
     spam = ['apples', 'bananas', 'tofu', 'cats']
@@ -22,3 +39,4 @@ if __name__ == '__main__':
 
     spam = []
     print(list_to_string(spam))
+
